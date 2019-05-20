7927e4cafd19b22b5286df2561d98c66419c7451
Austin Beaufort <tinyhatcoding@gmail.com>
2019-05-19 21:50:04 -0400

    Shuffle() function for arrays added to methods

--- index.js
+++ index.js
@@ -181,6 +181,13 @@ h = {
         return true;
     },
 
+    shuffle: function(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
 }
 
 
@@ -206,6 +213,5 @@ function makeReverseArray(firstNumber, secondNumber) {
     return rangeArray;
 }
 // ------------------------------------------------------------------------------------------
-
 
 module.exports = h;
